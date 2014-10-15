174170f22b7722896e481d882e9e6ecbac09f3b7
Maneesh Jha <maneesh08.jha@gmail.com>
2014-10-15 11:48:01 +0530

    Create IndustriesForm.java
    created IndustriesForm

---
+++ ComplianceKeyWeb/src/com/ck/DAO/Masters/IndustriesForm.java
@@ -0,0 +1,9 @@
+package com.ck.DAO.Masters; 
+
+import javax.servlet.http.HttpServletRequest; 
+import org.apache.struts.action.ActionForm; 
+import org.apache.struts.action.ActionMapping; 
+ 
+ 
+public class IndustriesForm extends ActionForm{ 
+}
