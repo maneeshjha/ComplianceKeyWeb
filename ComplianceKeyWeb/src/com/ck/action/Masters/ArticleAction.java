f6265496082e243eb66d237b85b6da39bd323a68
Maneesh Jha <maneesh08.jha@gmail.com>
2014-10-15 12:29:30 +0530

    Update and rename ArticleAction to ArticleAction.java

---
+++ ComplianceKeyWeb/src/com/ck/action/Masters/ArticleAction.java
@@ -0,0 +1,22 @@
+package com.ck.action.Masters;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.upload.FormFile;
+
+import com.ck.Bean.Masters.ArticleBean;
+import com.ck.DAO.Masters.ArticleForm;
+import com.ck.action.ComplianceDispatchAction;
+
+public class ArticleAction extends ComplianceDispatchAction{
+}
+	
